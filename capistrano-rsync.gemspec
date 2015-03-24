5dd9fd5a5264dee5cfc9f153751f09d03c4d7a65
Alexander Dormann <alexander.dormann@twt.de>
2015-03-24 11:25:28 +0100

    Styleguide fixes

--- capistrano-rsync.gemspec
+++ capistrano-rsync.gemspec
@@ -1,10 +1,10 @@
-require File.expand_path("../lib/capistrano/rsync/version", __FILE__)
+require File.expand_path('../lib/capistrano/rsync/version', __FILE__)
 
 Gem::Specification.new do |gem|
-  gem.name = "capistrano-rsync"
+  gem.name = 'capistrano-rsync'
   gem.version = Capistrano::Rsync::VERSION
-  gem.homepage = "https://github.com/moll/capistrano-rsync"
-  gem.summary = <<-end.strip.gsub(/\s*\n\s*/, " ")
+  gem.homepage = 'https://github.com/moll/capistrano-rsync'
+  gem.summary = <<-end.strip.gsub(/\s*\n\s*/, ' ')
     Deploy with Rsync from any local (or remote) repository.
     Capistrano v3 ready!
   end
@@ -19,14 +19,14 @@ Gem::Specification.new do |gem|
     Suitable for deploying any apps, be it Ruby or Node.js.
   end
 
-  gem.author = "Andri Möll"
-  gem.email = "andri@dot.ee"
-  gem.license = "LAGPL"
+  gem.author = 'Andri Möll'
+  gem.email = 'andri@dot.ee'
+  gem.license = 'LAGPL'
 
   gem.files = `git ls-files`.split($/)
   gem.executables = gem.files.grep(/^bin\//).map(&File.method(:basename))
   gem.test_files = gem.files.grep(/^spec\//)
-  gem.require_paths = ["lib"]
+  gem.require_paths = ['lib']
 
-  gem.add_dependency "capistrano", ">= 3.0.0.pre14", "< 4"
+  gem.add_dependency 'capistrano', '>= 3.0.0.pre14', '< 4'
 end
--- lib/capistrano/rsync.rb
+++ lib/capistrano/rsync.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../rsync/version", __FILE__)
+require File.expand_path('../rsync/version', __FILE__)
 
 # NOTE: Please don't depend on tasks without a description (`desc`) as they
 # might change between minor or patch version releases. They make up the
@@ -7,7 +7,7 @@ require File.expand_path("../rsync/version", __FILE__)
 
 rsync_cache = lambda do
   cache = fetch(:rsync_cache)
-  cache = deploy_to + "/" + cache if cache && cache !~ /^\//
+  cache = deploy_to + '/' + cache if cache && cache !~ /^\//
   cache
 end
 
@@ -15,34 +15,34 @@ end
 namespace :load do
   task :defaults do
     set :rsync_options, []
-    set :rsync_copy, "rsync --archive --acls --xattrs"
+    set :rsync_copy, 'rsync --archive --acls --xattrs'
 
     # Stage is used on your local machine for rsyncing from.
-    set :rsync_stage, "tmp/deploy"
+    set :rsync_stage, 'tmp/deploy'
 
     # Cache is used on the server to copy files to from to the release directory.
     # Saves you rsyncing your whole app folder each time.  If you nil rsync_cache,
     # Capistrano::Rsync will sync straight to the release path.
-    set :rsync_cache, "shared/deploy"
+    set :rsync_cache, 'shared/deploy'
   end
 end
 
-Rake::Task["deploy:check"].enhance ["rsync:hook_scm"]
-Rake::Task["deploy:updating"].enhance ["rsync:hook_scm"]
+Rake::Task['deploy:check'].enhance ['rsync:hook_scm']
+Rake::Task['deploy:updating'].enhance ['rsync:hook_scm']
 
-desc "Stage and rsync to the server (or its cache)."
+desc 'Stage and rsync to the server (or its cache).'
 task :rsync => %w[rsync:stage] do
   on roles(:all), in: :parallel do |host|
-    user = host.user + "@" if !host.user.nil?
+    user = host.user + '@' unless host.user.nil?
     port = host.port
 
     run_locally do
       rsynccmd = []
-      rsynccmd.concat(fetch(:rsync_options))
-      rsynccmd << fetch(:rsync_stage) + "/"
+      rsynccmd.concat fetch(:rsync_options)
+      rsynccmd << fetch(:rsync_stage) + '/'
       rsynccmd << "-e 'ssh -p #{port}'" if port
       rsynccmd << "#{user}#{host.hostname}:#{rsync_cache.call || release_path}"
-      execute :rsync, rsynccmd.join(" ")
+      execute :rsync, rsynccmd.join(' ')
     end
   end
 end
@@ -50,11 +50,11 @@ end
 namespace :rsync do
   task :hook_scm do
     Rake::Task.define_task("#{scm}:check") do
-      invoke "rsync:check"
+      invoke 'rsync:check'
     end
 
     Rake::Task.define_task("#{scm}:create_release") do
-      invoke "rsync:release"
+      invoke 'rsync:release'
     end
   end
 
@@ -63,27 +63,27 @@ namespace :rsync do
   end
 
   task :create_stage do
-    next if File.directory?(fetch(:rsync_stage))
+    next if File.directory? fetch(:rsync_stage)
 
     run_locally do
       execute :git, "clone #{fetch(:repo_url, '.')} #{fetch(:rsync_stage)}";
     end
 end
 
-  desc "Stage the repository in a local directory."
+  desc 'Stage the repository in a local directory.'
   task :stage => %w[create_stage] do
     run_locally do
       within fetch(:rsync_stage) do
-        execute :git, "fetch --quiet --all --prune"
+        execute :git, 'fetch --quiet --all --prune'
         execute :git, "reset --hard origin/#{fetch(:branch)}"
       end
     end
   end
 
-  desc "Copy the code to the releases directory."
+  desc 'Copy the code to the releases directory.'
   task :release => %w[rsync] do
     # Skip copying if we've already synced straight to the release directory.
-    next if !fetch(:rsync_cache)
+    next unless fetch(:rsync_cache)
 
     copy = %(#{fetch(:rsync_copy)} "#{rsync_cache.call}/" "#{release_path}/")
     on roles(:all), in: :parallel do
@@ -95,7 +95,7 @@ end
   # Plus was part of the public API in Capistrano::Rsync <= v0.2.1.
   task :create_release => %w[release]
 
-  desc "Set the current revision"
+  desc 'Set the current revision'
   task :set_current_revision do
     run_locally do
       within fetch(:rsync_stage) do
